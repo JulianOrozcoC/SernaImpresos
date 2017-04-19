e7c93b031c7013a328cab62f56b8c2ba118278d9
Julian Orozco <orozco.castorena@outlook.com>
2017-04-18 21:06:07 -0500

    Tab Panel in ordenes

--- ordenes.php
+++ ordenes.php
@@ -111,10 +111,31 @@
     <div id="page-wrapper">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Ordenes de Compra</h1>
+                <div class="container" style="width: 100%;"><h2>Ordenes de Compra</h2></div>
+
+                    <div id="exTab2" class="container"> 
+                    <ul class="nav nav-tabs">
+                        <li class="active">
+                            <a  href="#1" data-toggle="tab">Overview</a>
+                        </li>
+                        <li><a href="#2" data-toggle="tab">Without clearfix</a>
+                        </li>
+                        <li><a href="#3" data-toggle="tab">Solution</a>
+                        </li>
+                    </ul>
+
+            <div class="tab-content ">
+                <div class="tab-pane active" id="1">
+                    <h3>Standard tab panel created on bootstrap using nav-tabs</h3>
                 </div>
-                <img src="img/julygay.jpeg" alt="logo" height="570" width="520">
+            <div class="tab-pane" id="2">
+                <h3>Notice the gap between the content and tab after applying a background color</h3>
+            </div>
+            <div class="tab-pane" id="3">
+                <h3>add clearfix to tab-content (see the css)</h3>
+            </div>
+            </div>
+  </div>
 
 
 
